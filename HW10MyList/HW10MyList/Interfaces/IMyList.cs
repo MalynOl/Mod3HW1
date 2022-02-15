01b12af1b7c88fca07aeb4f010611c3237f308ca
Aleksandra <oleksandramalyn@gmail.com>
2022-02-15 12:09:53 +0200

    add myList

---
+++ HW10MyList/HW10MyList/Interfaces/IMyList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW10MyList
+{
+    public interface IMyList<T> : IEnumerable<T>
+    {
+        public void Add(T addItem);
+
+        public void AddRange(T[] collection);
+
+        public bool Remove(T item);
+
+        public void RemoveAt(int index);
+
+        public void Sort();
+    }
+}
